7787a86fb2678fe2f15cf721115ebd1fecc260de
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-03-08 14:19:07 -0300

    Created endpoint /posts at 'server' app, to return all Posts in database.

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 // import controller methods
-const {create} = require('../controllers/posts');
+const {create, list} = require('../controllers/posts');
 
 router.post('/post', create);
+router.get('/posts', list);
 
 module.exports = router; 
