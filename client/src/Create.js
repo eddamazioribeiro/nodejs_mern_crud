825fcb28b98828008bc5aba1034b4ddf92670cb5
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-03-08 12:04:24 -0300

    Using react state to handle de inputs values and creating handleChange function to do it.

--- client/src/Create.js
+++ client/src/Create.js
@@ -1,28 +1,51 @@
 import React, {useState} from 'react';
 
 const Create = () => {
-  return (
-  <div className="container p-5">
-    <h1>CREATE POST</h1>
-    <br/>
-    <form>
-        <div className="form-group">
-            <label className="text-muted">Title</label>
-            <input type="text" className="form-control" placeholder="Post title" required />
+    // state
+    const [state, setState] = useState({
+        title: '',
+        content: '',
+        user: ''
+    });
+
+    // destructure values from state
+    const{title, content, user} = state;
+
+    // onChange event handler
+    const handleChange = (name) => (event) => {
+        console.log('name', name, 'event', event.target.value);
+        setState({...state, [name]: event.target.value});
+    };
+
+    // * another way to write the same function above
+    // function handleChange(name){
+    //     return function(event){
+    //         setState({...state, [name]: event.target.value});
+    //     }
+    // }
+
+    return (
+        <div className="container p-5">
+            <h1>CREATE POST</h1>
+            <br/>
+            <form>
+                <div className="form-group">
+                    <label className="text-muted">Title</label>
+                    <input onChange={handleChange('title')} value={title} type="text" className="form-control" placeholder="Post title" required />
+                </div>
+                <div className="form-group">
+                    <label className="text-muted">Content</label>
+                    <textarea onChange={handleChange('content')} value={content} type="text" className="form-control" placeholder="Write something" required></textarea>
+                </div>
+                <div className="form-group">
+                    <label className="text-muted">User</label>
+                    <input onChange={handleChange('user')} value={user} type="text" className="form-control" placeholder="Your name" required />
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-primary">Create</button>
+                </div>        
+            </form>
         </div>
-        <div className="form-group">
-            <label className="text-muted">Title</label>
-            <textarea type="text" className="form-control" placeholder="Write something" required></textarea>
-        </div>
-        <div className="form-group">
-            <label className="text-muted">Title</label>
-            <input type="text" className="form-control" placeholder="Your name" required />
-        </div>
-        <div className="form-group">
-            <button className="btn btn-primary">Create</button>
-        </div>        
-    </form>
-  </div>
   );
 };
 
