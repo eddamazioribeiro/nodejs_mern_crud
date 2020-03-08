88055572a3688757f3b1ac10e5a96c07bb52a5d2
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-03-08 13:59:55 -0300

    Created 'Nav' component on 'client' app to navigate between pages.

--- client/src/Create.js
+++ client/src/Create.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import Nav from './Nav';
 require('dotenv').config();
 
 const Create = () => {
@@ -35,7 +36,9 @@ const Create = () => {
     } 
 
     return (
-        <div className="container p-5">
+        <div className="container pb-5">
+            <Nav />
+            <br/>
             <h1>CREATE POST</h1>
             <br/>
             <form onSubmit={handleSubmit}>
